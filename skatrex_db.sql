6f947f46fa4c68751747f78ce248fe232e26cca6
Seplaiz <77730495+Seplaiz@users.noreply.github.com>
2023-01-09 00:27:16 +0000

    Add files via upload

--- skatrex_db.sql
+++ skatrex_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 23-Nov-2022 às 12:29
--- Versão do servidor: 10.4.21-MariaDB
--- versão do PHP: 8.0.10
+-- Generation Time: Jan 09, 2023 at 01:26 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,25 +18,33 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Banco de dados: `skatrex_db`
+-- Database: `skatrex`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `category`
+-- Table structure for table `category`
 --
 
 CREATE TABLE `category` (
   `category_id` int(11) NOT NULL,
-  `category_name` int(11) NOT NULL,
-  `category_description` int(11) NOT NULL
+  `category_name` varchar(128) NOT NULL,
+  `category_description` varchar(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`category_id`, `category_name`, `category_description`) VALUES
+(1, 'Calçado', 'Calçados para estilo'),
+(2, 'Acessórios', 'Acessórios para estilo');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `orders`
+-- Table structure for table `orders`
 --
 
 CREATE TABLE `orders` (
@@ -49,37 +57,68 @@ CREATE TABLE `orders` (
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `order_details`
+-- Table structure for table `order_details`
 --
 
 CREATE TABLE `order_details` (
   `id` int(11) NOT NULL,
   `prod_id` int(11) NOT NULL,
   `order_id` int(11) NOT NULL,
-  `price` decimal(10,0) NOT NULL,
-  `quantity` int(11) NOT NULL
+  `price` float NOT NULL,
+  `quantity` int(64) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `product`
+-- Table structure for table `product`
 --
 
 CREATE TABLE `product` (
   `prod_id` int(11) NOT NULL,
   `category_id` int(11) NOT NULL,
   `prod_name` varchar(128) NOT NULL,
-  `prod_price` decimal(10,0) NOT NULL,
-  `img` int(255) NOT NULL,
+  `prod_price` float NOT NULL,
+  `img` varchar(255) NOT NULL,
   `stock` int(64) NOT NULL,
   `create_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`prod_id`, `category_id`, `prod_name`, `prod_price`, `img`, `stock`, `create_date`) VALUES
+(1, 1, 'DC SHOES MANUAL LE ELASTIC LACE SHOES KIDS', 49.99, 'DcShoesManualPreto.JPG', 5, '2023-01-07'),
+(2, 1, 'DC SHOES CRISIS 2 SKATE SHOES', 79.99, 'DcShoesCrisisPreto.JPG', 10, '2023-01-07'),
+(3, 1, 'DC SHOES PURE KIDS', 49.99, 'DcShoesPure.JPG', 10, '2023-01-07'),
+(4, 1, 'VANS BERLE PRO', 94.99, 'VansBerleProPreto.JPG', 5, '2023-01-07'),
+(5, 1, 'CARIUMA THE BERRICS', 119.99, 'CariumaTheBerrics.JPG', 10, '2023-01-07'),
+(6, 2, 'VANS BY CLASSIC PATCH HAT', 19.99, 'VansClassicHat.JPG', 7, '2023-01-07'),
+(7, 2, 'VISSLA DAGGER HAT', 34.99, 'VisslaDaggerHat.JPG', 2, '2023-01-07'),
+(8, 2, 'DC UPROAR GOLFER CLIPBACK HAT', 29.99, 'DcUproarGolferHat.JPG', 6, '2023-01-07'),
+(9, 2, 'CARHARTT PLAINS BUCKET HAT', 49.99, 'CarharttBucketHat.JPG', 2, '2023-01-07'),
+(10, 2, 'CARHARTT JAKE SHOULDER POUCH BAG', 39.99, 'CarharttJakePouchBag.JPG', 8, '2023-01-07'),
+(11, 2, 'MERGE4 PINK CAMO', 14.99, 'Merger4PinkCamo.JPG', 13, '2023-01-07'),
+(12, 2, 'MERGE4 HAVEN TALL BLUE', 14.99, 'Merge4HavenTallBlue.JPG', 8, '2023-01-07'),
+(13, 2, 'MERGE4 CHECK YELLOW / BLACK', 14.99, 'Merge4CheckYellowBlack.JPG', 4, '2023-01-07');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_categories`
+--
+
+CREATE TABLE `product_categories` (
+  `id` int(11) NOT NULL,
+  `prod_id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `user`
+-- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
@@ -93,73 +132,114 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Índices para tabelas despejadas
+-- Indexes for dumped tables
 --
 
 --
--- Índices para tabela `category`
+-- Indexes for table `category`
 --
 ALTER TABLE `category`
   ADD PRIMARY KEY (`category_id`);
 
 --
--- Índices para tabela `orders`
+-- Indexes for table `orders`
 --
 ALTER TABLE `orders`
-  ADD PRIMARY KEY (`order_id`);
+  ADD PRIMARY KEY (`order_id`),
+  ADD KEY `UserOrder` (`user_id`);
 
 --
--- Índices para tabela `order_details`
+-- Indexes for table `order_details`
 --
 ALTER TABLE `order_details`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `order_details_orderid` (`order_id`),
+  ADD KEY `order_details_prodid` (`prod_id`);
 
 --
--- Índices para tabela `product`
+-- Indexes for table `product`
 --
 ALTER TABLE `product`
   ADD PRIMARY KEY (`prod_id`);
 
 --
--- Índices para tabela `user`
+-- Indexes for table `product_categories`
+--
+ALTER TABLE `product_categories`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `product_categories_categoryid` (`category_id`),
+  ADD KEY `product_categories_prodid` (`prod_id`);
+
+--
+-- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`user_id`),
   ADD UNIQUE KEY `Emails` (`email`);
 
 --
--- AUTO_INCREMENT de tabelas despejadas
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT de tabela `category`
+-- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT de tabela `orders`
+-- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
   MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de tabela `order_details`
+-- AUTO_INCREMENT for table `order_details`
 --
 ALTER TABLE `order_details`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de tabela `product`
+-- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `prod_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `prod_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
--- AUTO_INCREMENT de tabela `user`
+-- AUTO_INCREMENT for table `product_categories`
+--
+ALTER TABLE `product_categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
   MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `UserOrder` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+
+--
+-- Constraints for table `order_details`
+--
+ALTER TABLE `order_details`
+  ADD CONSTRAINT `order_details_orderid` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`),
+  ADD CONSTRAINT `order_details_prodid` FOREIGN KEY (`prod_id`) REFERENCES `product` (`prod_id`);
+
+--
+-- Constraints for table `product_categories`
+--
+ALTER TABLE `product_categories`
+  ADD CONSTRAINT `product_categories_categoryid` FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`),
+  ADD CONSTRAINT `product_categories_prodid` FOREIGN KEY (`prod_id`) REFERENCES `product` (`prod_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
