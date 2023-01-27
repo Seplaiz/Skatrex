3b9213d51a00c581d8cbdeae55e4b56ad50c2791
Seplaiz <77730495+Seplaiz@users.noreply.github.com>
2023-01-27 13:12:01 +0000

    Add files via upload

--- index.php
+++ index.php
@@ -139,7 +139,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta http-equiv="x-ua-compatible" content="ie=edge" />
     <title>Skatrex Store</title>
-    <link rel="icon" type="image/x-icon" href="/img/skatrexlogo.ico">
+    <link rel="icon" href="img\skatrexlogo.ico" type="image/x-icon" />
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" />
     <!-- Google Fonts Roboto -->
@@ -280,62 +280,59 @@
 
       <!--Section: Content-->
       <section class="text-center">
-        <h4 class="mb-5"><strong>Facilis consequatur eligendi</strong></h4>
+        <h4 class="mb-5"><strong>Vans Destaques</strong></h4>
 
         <div class="row">
           <div class="col-lg-4 col-md-12 mb-4">
-            <div class="card">
+            <div class="card h-100">
               <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
-                <img src="https://mdbootstrap.com/img/new/standard/nature/184.jpg" class="img-fluid" />
+                <img src="img\VansClassicHat.jpg" width="50%" height="50%" class="img-fluid" />
                 <a href="#!">
                   <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
                 </a>
               </div>
               <div class="card-body">
-                <h5 class="card-title">Card title</h5>
+                <h5 class="card-title">Vans Classic Hat</h5>
                 <p class="card-text">
-                  Some quick example text to build on the card title and make up the bulk of the
-                  card's content.
+                  19.99€
                 </p>
-                <a href="#!" class="btn btn-primary">Button</a>
+                <a href="#!" class="btn btn-primary">Comprar</a>
               </div>
             </div>
           </div>
 
           <div class="col-lg-4 col-md-6 mb-4">
-            <div class="card">
+            <div class="card h-100">
               <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
-                <img src="https://mdbootstrap.com/img/new/standard/nature/023.jpg" class="img-fluid" />
+                <img src="img\VansBerleProPreto.jpg" width="50%" height="50%" class="img-fluid" />
                 <a href="#!">
                   <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
                 </a>
               </div>
               <div class="card-body">
-                <h5 class="card-title">Card title</h5>
+                <h5 class="card-title">Vans Berle Pro</h5>
                 <p class="card-text">
-                  Some quick example text to build on the card title and make up the bulk of the
-                  card's content.
+                  94.99€
                 </p>
-                <a href="#!" class="btn btn-primary">Button</a>
+                <a href="#!" class="btn btn-primary">Comprar</a>
               </div>
             </div>
           </div>
 
           <div class="col-lg-4 col-md-6 mb-4">
-            <div class="card">
+            <div class="card h-100">
               <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
-                <img src="https://mdbootstrap.com/img/new/standard/nature/111.jpg" class="img-fluid" />
+                <img src="img\VansMeiasCheckerboard.jpg" width="100" height="100" class="img-fluid" />
                 <a href="#!">
                   <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
                 </a>
               </div>
               <div class="card-body">
-                <h5 class="card-title">Card title</h5>
+                <h5 class="card-title">Vans Meias Checkerboard</h5>
                 <p class="card-text">
-                  Some quick example text to build on the card title and make up the bulk of the
-                  card's content.
+                  14.99€
                 </p>
-                <a href="#!" class="btn btn-primary">Button</a>
+                <a href="#!" class="btn btn-primary">Comprar</a>
               </div>
             </div>
           </div>
